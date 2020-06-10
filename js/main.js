0cccab285d3c12c29df6cd4e4f8edf69b9929544
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-06-10 17:29:05 +0300

    adaptive done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
+// Header + Burger menu
+
 (function () {
     const header = document.querySelector('.header');
+    const burgerMenu = document.querySelector('.header-burger__menu');
+    const headerMenu = document.querySelector('.header-nav');
+    const body = document.querySelector('body');
+    const headerMenuLinks = document.querySelectorAll('.header-nav__link');
+
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
+            headerMenu.classList.add('scroll');
         } else {
             header.classList.remove('header_active');
+            headerMenu.classList.remove('scroll');
         }
     };
+
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        headerMenu.classList.toggle('active');
+        body.classList.toggle('lock');
+    });
+
+    if (window.innerWidth <= 990) {
+        for (let i = 0; i < headerMenuLinks.length; i += 1) {
+            headerMenuLinks[i].addEventListener('click', () => {
+                burgerMenu.classList.remove('active');
+                headerMenu.classList.remove('active');
+            });
+        }
+    }
+
 }());
+
