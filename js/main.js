861db4088dbef4431328a21482f2a9ea20f147c9
Dmitry Petrov <61933931+dmitrypetrov-dev@users.noreply.github.com>
2020-06-10 20:48:12 +0300

    all done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-// Header + Burger menu
+const header = document.querySelector('.header');
+const burgerMenu = document.querySelector('.header-burger__menu');
+const headerMenu = document.querySelector('.header-nav');
+const body = document.querySelector('body');
+const headerMenuLinks = document.querySelectorAll('.header-nav__link');
+
 
 (function () {
-    const header = document.querySelector('.header');
-    const burgerMenu = document.querySelector('.header-burger__menu');
-    const headerMenu = document.querySelector('.header-nav');
-    const body = document.querySelector('body');
-    const headerMenuLinks = document.querySelectorAll('.header-nav__link');
-
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
             header.classList.add('header_active');
@@ -16,21 +15,62 @@
             headerMenu.classList.remove('scroll');
         }
     };
-
     burgerMenu.addEventListener('click', () => {
         burgerMenu.classList.toggle('active');
         headerMenu.classList.toggle('active');
         body.classList.toggle('lock');
     });
+}());
 
+// Scroll to anchors
+
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+(function () {
     if (window.innerWidth <= 990) {
         for (let i = 0; i < headerMenuLinks.length; i += 1) {
             headerMenuLinks[i].addEventListener('click', () => {
                 burgerMenu.classList.remove('active');
                 headerMenu.classList.remove('active');
+                body.classList.remove('lock');
             });
         }
     }
-
 }());
-
